c7f7322a14e9c171efbaeebdeb55b0319ae89046
Sazzad <sazzad47.ju@gmail.com>
2023-05-02 08:55:12 -0700

    make it better

--- src/Layouts/index.js
+++ src/Layouts/index.js
@@ -8,6 +8,7 @@ import withRouter from "../Components/Common/withRouter";
 import Header from "./Header";
 import Sidebar from "./Sidebar";
 import Footer from "./Footer";
+import { useEffect } from "react";
 
 const Layout = (props) => {
   const [isOpenSidebar, setIsOpenSidebar] = useState(true);
@@ -16,17 +17,32 @@ const Layout = (props) => {
     setIsOpenSidebar(!isOpenSidebar);
   };
 
+  useEffect(() => {
+    const handleResize = () => {
+      if (window.innerWidth < 980) {
+        setIsOpenSidebar(false);
+      } else {
+        setIsOpenSidebar(true);
+      }
+    };
+
+    window.addEventListener("resize", handleResize);
+
+    // Remove event listener on unmount
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
+
   return (
     <React.Fragment>
       <Container className="app_container">
         <Header toggleSidebar={toggleSidebar} />
         <div className={`wrapper ${isOpenSidebar ? "toggled" : ""}`}>
-            <Sidebar isOpenSidebar={isOpenSidebar} />
-            <div className="page-content-wrapper">
-              {props.children}
-              <Footer/>
-            </div>
+          <Sidebar isOpenSidebar={isOpenSidebar} />
+          <div className="page-content-wrapper">
+            {props.children}
+            <Footer />
           </div>
+        </div>
       </Container>
     </React.Fragment>
   );
